7d3f37a6c7a555328394240b5e96c5691b302c23
LogeshAD <deepalogesh7@gmail.com>
2024-05-13 09:26:05 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,58 @@
+
+
+var result = document.getElementById('res')
+
+
+
+function add() {
+
+    var a = document.getElementById('num1')
+    var b = document.getElementById('num2')
+    var inbox1 = Number(a.value)
+    var outbox2 = Number(b.value)
+    var total = inbox1 + outbox2
+
+
+    console.log(total)
+    result.textContent = "The result is: " + total
+
+}
+function sub() {
+
+    var a = document.getElementById('num1')
+    var b = document.getElementById('num2')
+    var inbox1 = Number(a.value)
+    var outbox2 = Number(b.value)
+    var total = inbox1 - outbox2
+
+
+    console.log(total)
+    result.textContent = "The result is: " + total
+
+}
+function mul() {
+
+    var a = document.getElementById('num1')
+    var b = document.getElementById('num2')
+    var inbox1 = Number(a.value)
+    var outbox2 = Number(b.value)
+    var total = inbox1 * outbox2
+
+
+    console.log(total)
+    result.textContent = "The result is: " + total
+
+}
+function div() {
+
+    var a = document.getElementById('num1')
+    var b = document.getElementById('num2')
+    var inbox1 = Number(a.value)
+    var outbox2 = Number(b.value)
+    var total = inbox1 / outbox2
+
+
+    console.log(total)
+    result.textContent = "The result is: " + total
+
+}
